aec24b1422c9fd0e5617f5383687c1d5566617a0
Vishal N <61023404+vishalnandagopal@users.noreply.github.com>
2023-02-27 23:19:29 +0530

    fix kavya's name not appearing and few other stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,71 @@
-(function($) {
+(function ($) {
+    "use strict";
 
-	"use strict";
-	
-
-
-	// Cache selectors
+    // Cache selectors
     var lastId,
         topMenu = $("#main-menu"),
-        topMenuHeight = topMenu.outerHeight()+15,
+        topMenuHeight = topMenu.outerHeight() + 15,
         // All list items
         menuItems = topMenu.find("a"),
         // Anchors corresponding to menu items
-        scrollItems = menuItems.map(function(){
-          var item = $($(this).attr("href"));
-          if (item.length) { return item; }
+        scrollItems = menuItems.map(function () {
+            var item = $($(this).attr("href"));
+            if (item.length) {
+                return item;
+            }
         });
 
     // Bind click handler to menu items
-	// so we can get a fancy scroll animation
-    menuItems.click(function(e){
-      var href = $(this).attr("href"),
-          offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-      $('html, body').stop().animate({ 
-          scrollTop: offsetTop
-      }, 300);
-      e.preventDefault();
+    // so we can get a fancy scroll animation
+    menuItems.click(function (e) {
+        var href = $(this).attr("href"),
+            offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+        $("html, body").stop().animate(
+            {
+                scrollTop: offsetTop,
+            },
+            300
+        );
+        e.preventDefault();
     });
-	// Bind to scroll
-    $(window).scroll(function(){
-       // Get container scroll position
-       var fromTop = $(this).scrollTop()+topMenuHeight;
-       
-       // Get id of current scroll item
-       var cur = scrollItems.map(function(){
-         if ($(this).offset().top < fromTop)
-           return this;
-       });
-       // Get the id of the current element
-       cur = cur[cur.length-1];
-       var id = cur && cur.length ? cur[0].id : "";
-       
-       if (lastId !== id) {
-           lastId = id;
-           // Set/remove active class
-           menuItems
-             .parent().removeClass("active")
-             .end().filter("[href=#"+id+"]").parent().addClass("active");
-       }                   
+    // Bind to scroll
+    $(window).scroll(function () {
+        // Get container scroll position
+        var fromTop = $(this).scrollTop() + topMenuHeight;
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function () {
+            if ($(this).offset().top < fromTop) return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+            lastId = id;
+            // Set/remove active class
+            menuItems
+                .parent()
+                .removeClass("active")
+                .end()
+                .filter("[href=#" + id + "]")
+                .parent()
+                .addClass("active");
+        }
     });
 
+    $("#Grid").mixitup();
 
-
-
-    $('#Grid').mixitup();
-
-
-
-    $(function(){
+    $(function () {
         $('[data-rel="lightbox"]').lightbox();
     });
-
-
-
-
-
 })(jQuery);
+
+// Old timeline.js
+
+timeline(document.querySelectorAll(".timeline"), {
+    forceVerticalMode: 700,
+    mode: "horizontal",
+    verticalStartPosition: "left",
+    visibleItems: 4,
+});
--- assets/js/timeline.js
+++
@@ -1,6 +0,0 @@
-    timeline(document.querySelectorAll('.timeline'), {
-      forceVerticalMode: 700,
-      mode: 'horizontal',
-      verticalStartPosition: 'left',
-      visibleItems: 4
-    });
